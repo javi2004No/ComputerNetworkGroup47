aeea99e5df662769692336fe254c0d0bf36470b7
javi2004No <102776054+javi2004No@users.noreply.github.com>
2026-03-08 19:55:24 -0400

    Added the main functionality of the class to be able to pick its preferred neighbors.

--- memory/memory_main.py
+++ memory/memory_main.py
@@ -22,6 +22,7 @@ class memory_main:
         self._chunkSize = chunkSize
         self._interval = interval
         self._windowSize = num_of_preferred_neighbors
+        self._optimistic_neighbor = -1;
 
     def add_neighbor(self, name, chunks):
         """
@@ -68,22 +69,67 @@ class memory_main:
     def calculate_download_rate(self, downloads):
         """
         Calculates the download rates of all neighbors.
-        :param downloads: The amount of data that was downloaded from the neighbors.
+        :param downloads: The amount of data that was downloaded from the neighbors. Assumes it's a 2d array with the
+        second value in the inner array being the size of the download.
         :return: A list of the download rates.
         """
-        return [download/self._interval for download in downloads]
+        return [download[1]/self._interval for download in downloads]
 
-    def pick_random_n(self, num, values):
+    def pick_random_n(self, num, arr):
         """
-        Picks
-        :param num:
-        :param values:
-        :return:
+        Picks a number of values randomly.
+        :param num: how many values should be picked.
+        :param arr: the values to pick from.
+        :return: list of a random selection of values from values.
         """
         ans = []
         for i in range(num):
-            ans.append(values.pop(random.randint(0, len(values)-1)))
+            ans.append(arr.pop(random.randint(0, len(arr)-1)))
         return ans
+
+    def pick_preferred_neighbors(self, downloads):
+        """
+        Picks the new preferred_neighbors.
+        :param downloads: the id of the neighbors followed by the amount of downloads of said neighbor.
+        :return: a tuple containing where the first element contains a list of neighbors to unchoke and the second
+        contains a list of neighbors to choke.
+        """
+        to_unchoke = []
+        # Will use the download rates to pick its preferred neighbors if the bitmap is not complete otherwise it will
+        # just pick randomly
+        if not self._bitmap.complete():
+            download_rates = calculate_download_rate(downloads)
+            download_rates.sort(reverse = True)
+            cur = 0
+            while cur < self._windowSize:
+                i = cur
+                rate = download_rates[i]
+                while i < len(download_rates) and rate == download_rates[i]:
+                    i += 1
+                downloads_selected = []
+                if i > self._windowSize:
+                    downloads_selected = self.pick_random_n(i - cur, downloads[cur:i])
+                else:
+                    downloads_selected = downloads[cur:i]
+                for download in downloads_selected:
+                    to_unchoke.append(download[0])
+                cur = i
+        else:
+            downloads_selected = self.pick_random_n(self._windowSize, downloads)
+            for download in downloads_selected:
+                to_unchoke.append(download[0])
+        # Only returns the changing neighbors e.g neighbors that are choked and need to be unchoked and vice versa.
+        choke = []
+        unchoke = []
+        for id, data in self._neighbors:
+            if data.choked and id not in to_unchoke:
+                choke.append(id)
+            elif not data.choked and id in to_unchoke:
+                unchoke.append(id)
+        return unchoke, choke
+
+
+
 
 
 
