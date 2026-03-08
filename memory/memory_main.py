fb9edadbeee53606fbd846e39469cded08537db1
javi2004No <102776054+javi2004No@users.noreply.github.com>
2026-03-08 15:21:11 -0400

    Added functionality to the memory_main class which will function as the main holder for our bitmaps.

--- memory/memory_main.py
+++ memory/memory_main.py
@@ -1,2 +1,56 @@
+import mem_File
 class memory_main:
-    pass
+
+    def __init__(self, hasFile, fileSize, chunkSize):
+        """
+        Set up the memeory object with the current bitmap.
+        :param hasFile: Weather the host has the bitmap or not. 1 if the host has the bitmap. 0 if not.
+        :param fileSize: The size of the file.
+        :param chunkSize: The size of the chunks separating the file.
+        """
+        self._bitMap = mem_File(hasFile, fileSize, chunkSize, [])
+        self._neighbors = {}
+        self._fileSize = fileSize
+        self._chunkSize = chunkSize
+
+    def add_neighbor(self, name, chunks):
+        """
+        Creates a bitmap for the neighbor specified by name.
+        :param name: The ID of the neighbor.
+        :param chunks: The chunks that the neighbor contains.
+        """
+        self._neighbors[name] = mem_File(0, self._fileSize, self._chunkSize, chunks)
+
+    def update_neighbor(self, name, indexes, chunks):
+        """
+        Updates the bitmap of the neighbor.
+        :param name: The ID of the neighbor.
+        :param indexes: The indexes of chunks to update.
+        :param chunks: The chunks to replace them with.
+        """
+        self._neighbors[name].update(indexes, chunks)
+
+    def update_bitmap(self, index, chunk):
+        """
+        Updates a chunk in the current bitmap.
+        :param index: The index of the chunk to update.
+        :param chunk: What to update the chunk to.
+        """
+        self._bitMap.update([index], [chunk])
+
+    def interest(self, neighbor):
+        """
+        Returns what chunks our current client would be interested in from its neighbor. e.g. what chunks do they have
+        that we don't.
+        :param neighbor: The id of the neighbor we are interested in.
+        :return: A list of chunks that we can request from the neighbor.
+        """
+        return list(set(self._bitMap.havePieces()) - set(self._neighbors[neighbor].havePieces()))
+
+    def all_interests(self):
+        """
+        Get interests for all the neighbors.
+        :return: A list of interests from all neighbors.
+        """
+        return [interest(neighbor) for neighbor in self._neighbors.keys()]
+
