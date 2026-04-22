c9b968271762a756af45b70f61c9229fa615a7f3
javi2004No <102776054+javi2004No@users.noreply.github.com>
2026-04-22 00:07:41 -0400

    thread safety issues.

--- utils/logger.py
+++ utils/logger.py
@@ -1,4 +1,5 @@
 import datetime
+import threading
 
 
 def _get_time():
@@ -9,54 +10,66 @@ class logger:
     def __init__(self, id):
         self._id = id
         self._file = open("log_peer_" + str(id) + ".log", "w")
+        self._lock = threading.Lock()
 
     def log_tcp_connection(self, id_to, connected_to):
-        if connected_to:
-            self._file.write(
-                _get_time() + "Peer " + str(self._id) + " makes a connection to Peer " + str(id_to) + ".\n")
-        else:
-            self._file.write(
-                _get_time() + "Peer " + str(self._id) + " is connected from Peer " + str(id_to) + ".\n")
+        with self._lock:
+            if connected_to:
+                self._file.write(
+                    _get_time() + "Peer " + str(self._id) + " makes a connection to Peer " + str(id_to) + ".\n")
+            else:
+                self._file.write(
+                    _get_time() + "Peer " + str(self._id) + " is connected from Peer " + str(id_to) + ".\n")
 
     def log_change_of_preferred_neighbors(self, preferred_neighbors):
-        to_write = _get_time() + "Peer " + str(self._id) + "has the preferred neighbors"
-        if preferred_neighbors:
-            to_write += " " + str(preferred_neighbors[0])
-            for neighbor in preferred_neighbors[1:]:
-                to_write += ", " + str(neighbor)
-        to_write += "."
-        self._file.write(to_write)
+        with self._lock:
+            to_write = _get_time() + "Peer " + str(self._id) + "has the preferred neighbors"
+            if preferred_neighbors:
+                to_write += " " + str(preferred_neighbors[0])
+                for neighbor in preferred_neighbors[1:]:
+                    to_write += ", " + str(neighbor)
+            to_write += "."
+            self._file.write(to_write)
 
     def log_change_of_optimistically_unchoked_neighbor(self, id_to):
-        self._file.write(
-            _get_time() + "Peer " + str(self._id) + " has the optimistically unchoked neighbor " + str(id_to) + ".\n")
+        with self._lock:
+            self._file.write(
+                _get_time() + "Peer " + str(self._id) + " has the optimistically unchoked neighbor " + str(id_to) + ".\n")
 
     def log_unchoked(self, id_from):
-        self._file.write(_get_time() + "Peer " + str(self._id) + " is unchoked by " + str(id_from) + ".\n")
+        with self._lock:
+            self._file.write(_get_time() + "Peer " + str(self._id) + " is unchoked by " + str(id_from) + ".\n")
 
     def log_choked(self, id_from):
-        self._file.write(_get_time() + "Peer " + str(self._id) + " is choked by " + str(id_from) + ".\n")
+        with self._lock:
+            self._file.write(_get_time() + "Peer " + str(self._id) + " is choked by " + str(id_from) + ".\n")
 
     def log_received_have(self, id_from, piece_index):
-        self._file.write(_get_time() + "Peer " + str(self._id) + " received the 'have' message from " + str(id_from)
-                         + " for the piece " + str(piece_index) + ".\n")
+        with self._lock:
+            self._file.write(_get_time() + "Peer " + str(self._id) + " received the 'have' message from " + str(id_from)
+                             + " for the piece " + str(piece_index) + ".\n")
 
     def log_received_interested(self, id_from):
-        self._file.write(
-            _get_time() + "Peer " + str(self._id) + " received the 'interested' message from  " + str(id_from) + ".\n")
+        with self._lock:
+            self._file.write(
+                _get_time() + "Peer " + str(self._id) + " received the 'interested' message from  " + str(id_from) + ".\n")
 
     def log_received_not_interested(self, id_from):
-        self._file.write(
-            _get_time() + "Peer " + str(self._id) + " received the 'not interested' message from  " + str(id_from)
-            + ".\n")
+        with self._lock:
+            self._file.write(
+                _get_time() + "Peer " + str(self._id) + " received the 'not interested' message from  " + str(id_from)
+                + ".\n")
 
     def log_downloaded_piece(self, id_from, piece_index, number_of_pieces):
-        self._file.write(_get_time() + "Peer " + str(self._id) + " has downloaded the piece " + str(piece_index) +
-                         " from " + str(id_from) + ". Now the number of pieces it has is " + str(number_of_pieces)
-                         + ".\n")
+        with self._lock:
+            self._file.write(_get_time() + "Peer " + str(self._id) + " has downloaded the piece " + str(piece_index) +
+                             " from " + str(id_from) + ". Now the number of pieces it has is " + str(number_of_pieces)
+                             + ".\n")
 
     def log_completed_download(self):
-        self._file.write(_get_time() + "Peer " + str(self._id) + " has downloaded the complete file.\n")
+        with self._lock:
+            print("WHATTTTTTTTTTTTTTTTTTTTTTTTTT")
+            self._file.write(_get_time() + "Peer " + str(self._id) + " has downloaded the complete file.\n")
 
     def close(self):
         self._file.close()
